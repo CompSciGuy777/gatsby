e9ce815d674419c720cb61814d97ec6c6a1aae4e
Christopher Davis <compsciguy777@gmail.com>
2018-11-26 12:34:14 -0500

    the third commit

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,7 +5,8 @@ export default function Template({data}) {
     const {markdownRemark: post} = data;
     return(
         <div>
-            <h1>{post.frontmatter.title}</h1>
+            <Helmet title={`{post.frontmatter.title} | {siteTitle}`} />
+            <h1>{`{post.frontmatter.title}`}</h1>
             <div dangerouslySetInnerHTML={{__html: post.html}} />
         </div>
     )
